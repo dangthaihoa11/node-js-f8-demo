94c77beb7346235164f74de53369b9d6f8a79d51
hoadang <dangthaihoa1105@gmail.com>
2022-08-30 17:00:32 +0700

    update hbs and scss

--- nodejs_blog/src/index.js
+++ nodejs_blog/src/index.js
@@ -1,9 +1,28 @@
+const path = require('path')
 const express = require('express')
-const app = express()
+const morgan = require('morgan')
+const hbs = require('express-handlebars');
+const app = express();
 const port = 3000
 
+// static
+
+app.use(express.static(path.join(__dirname,'public')))
+
+// HTTTP logger
+
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', hbs.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname,'resources/views'));
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
 })
 
 app.listen(port, () => {
