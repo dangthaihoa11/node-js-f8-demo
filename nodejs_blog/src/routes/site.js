e2b3fde87e1d5526cf9af7d578e029af6c13a52f
dangthaihoa11 <dangthaihoa1105@gmail.com>
2022-09-02 16:51:55 +0700

    update route and controller

---
+++ nodejs_blog/src/routes/site.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const siteController = require('../app/controllers/siteController.js');
+
+// NewsController.index
+
+
+router.get('/search', siteController.search);
+router.get('/', siteController.index);
+
+module.exports = router;
+
